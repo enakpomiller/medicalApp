7331d4dce01de222f25a20a2fe416bbf440f8bb2
enakpomiller <63649279+enakpomiller@users.noreply.github.com>
2023-08-05 15:13:21 +0100

    button correction

--- application/views/layout/fragments/template.php
+++ application/views/layout/fragments/template.php
@@ -1,12 +1,12 @@
    
    
    <!-- ***** Breadcumb Area Start ***** -->
-    <section class="breadcumb-area bg-img gradient-background-overlay" style="background-image: url(<?=base_url()?>assets/img/bg-img/breadcumb1.jpg);">
+    <section class="breadcumb-area bg-img gradient-background-overlay" style="background-image: url(<?=base_url()?>assets/img/bg-img/breadcumb1.jpg);height:260px;">
         <div class="container h-100">
             <div class="row h-100 align-items-center">
                 <div class="col-12">
                     <div class="breadcumb-content">
-                        <h3 class="breadcumb-title">Good Medicals</h3>
+                        <h4 class="breadcumb-title">Good Medicals</h4>
                         <p> We guarantee you good health</p>
                     </div>
                 </div>
--- application/views/layout/pages/view_result.php
+++ application/views/layout/pages/view_result.php
@@ -23,7 +23,7 @@
                             <th scope="row"><?=$counter++;?></th>
                             <td><?=$row->firstname?></td>
                             <td><?=$row->othernames?></td>
-                            <td><?=$row->status==1?'<div class="text-primary">Result Approved</div>':'<div class="text-danger">not approved</div>'?></td>
+                            <td><?=$row->status==1?'<div class="text-success">Result Approved</div>':'<div class="text-danger">not approved</div>'?></td>
                             <td style="float:right;"> 
                             <a href="<?=base_url('home/print/'.$row->id)?>" class="appoint-btn bg-primary pl-4 pr-2 text-white pt-2 pb-2"><i class="fa fa-eye"></i> View Result </a>
                               <a href="<?=base_url('home/delete_patient_rec/'.$row->id)?>" onclick="return confirm(' Action Will Delete!')"  class="appoint bg-danger pl-2 pr-2 text-white pt-2 pb-2 text-end"><i class="fa fa-trash"></i> Delete  </a>
@@ -38,9 +38,9 @@
                             <h5 style="position:relative;bottom:60px;">ooPs!!!<br> Doctor Has not prescribe drug for patient!</h5>
                             <a href="<?=site_url('home/patient_reg')?>" class="btn  medilife-appoint-btn" style="position:relative;bottom:60px;background:#006cff;color:white;"> Refresh </a>
                         </div>
-                   
-                    <?php } ?>
+                <?php } ?>
             </div>
+            <a href="<?=site_url('home/patient_reg')?>" class="medilife-appoint btn-dark pt-2 pb-2 pl-4 pr-4"><i class="fa fa-backward"></i> Prev </a>
         </div>
     </section>
 
